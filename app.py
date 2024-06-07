ed5317007c5236c6bb63ec14d53a3bfcdd500930
Naveen J <naveenlenin934.com>
2024-06-07 20:06:55 +0530

    added search option

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import google.generativeai as genai
 from langchain_community.llms.huggingface_endpoint import HuggingFaceEndpoint
 from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.prompts import PromptTemplate
 import os
 
 st.set_page_config(page_title="AI Tool", page_icon=":robot:")
@@ -24,23 +25,26 @@ def get_api():
 
 
 
+def hugging_llm(model,api):
+    os.environ["HUGGINGFACEHUB_API_TOKEN"]=api
+    llm=HuggingFaceEndpoint(repo_id=model)
+    response=llm.invoke(prompt)
+    return response
+
+
 def get_answer(api_key):
     if model=="mistal":
-        model_name="mistralai/Mistral-7B-Instruct-v0.3"      
-        os.environ["HUGGINGFACEHUB_API_TOKEN"]=api_key
-        llm=HuggingFaceEndpoint(repo_id=model_name)
-        response=llm.invoke(question)
+        model_name="mistralai/Mistral-7B-Instruct-v0.3"
+        response=hugging_llm(model_name,api_key)      
     elif model=="llama":
         model_name="meta-llama/Meta-Llama-3-8B-Instruct"
-        os.environ["HUGGINGFACEHUB_API_TOKEN"]=api_key
-        llm=HuggingFaceEndpoint(repo_id=model_name)
-        response=llm.invoke(question)
+        response=hugging_llm(model_name,api_key)
     elif model=="Gemini":
         st.session_state["api_key"]=st.sidebar.text_input("please ender your Google API",type='password')
         os.environ['GOOGLE_API_KEY']=api_key
         genai.configure(api_key=os.environ['GOOGLE_API_KEY'])
         llm=ChatGoogleGenerativeAI(model="gemini-pro",temperature=0.3)
-        response=llm.invoke(question).content
+        response=llm.invoke(prompt).content
 
     return response
 
@@ -48,14 +52,20 @@ def get_answer(api_key):
 api=get_api()
 if api:
   st.write("getting API")
-question=st.text_input("Ask you questions")
-button2=st.button("summit")
+  question=st.text_input("Ask you questions")
+  button2=st.button("summit")
+
+from langchain_community.tools import DuckDuckGoSearchRun
+searchduck= DuckDuckGoSearchRun()
+search=searchduck.run(question)
+
+template="You are AI assitance give related answer to users question, that question is {question} if user ask question from current affair use the duckduck go search result as content, that duckduck go search is {search}"
+example_promt=PromptTemplate(input_variables=["question","search"],template=template)
+prompt=example_promt.format(question=question,search=search)
 
 if button2:    
     response=get_answer(st.session_state["api_key"])
     st.write(response)
-
-
 
 
 
